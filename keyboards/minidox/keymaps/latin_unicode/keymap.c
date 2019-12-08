b3d2bd178c32f8dd8a9e6fea05cdc02e7bdb2c9e
squizzler <56064906+squizzler@users.noreply.github.com>
2019-12-08 23:59:28 +0000

    Unicode map imported
    Found that the folder still contained default keymap from when it was spawned. Fixed

--- keyboards/minidox/keymaps/latin_unicode/keymap.c
+++ keyboards/minidox/keymaps/latin_unicode/keymap.c
@@ -1,4 +1,8 @@
+//Knocked about version of the Norman layout from the Minidox layouts folder. Uses RSTHD keymap.
+//Possibly available to view on http://www.keyboard-layout-editor.com
+
 #include QMK_KEYBOARD_H
+
 
 extern keymap_config_t keymap_config;
 
@@ -6,157 +10,95 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+
+enum layers {
+    _RSTHD,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  RSTHD = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
+  ADJUST
 };
 
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
+#define DSK_LFT LGUI(LCTL(KC_LEFT))
+#define DSK_RT  LGUI(LCTL(KC_RIGHT))
+
+
+enum unicode_names {
+  COMA,
+  QUEST,
+  FULST,
+  EXCLA,
+  LTBRK,
+  RTBRK,
+  MORTHN,
+  LESTHN,
+  CIRCUM,
+  SECT,
+  PLUMIN,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [COMA]  = 0x002C,  // ,
+  [QUEST] = 0x003F,
+  [FULST] = 0x002E,
+  [EXCLA] = 0x0021,
+  [LTBRK] = 0x0028,
+  [RTBRK] = 0x0029,
+  [MORTHN]= 0x003C,
+  [LESTHN]= 0x003E,
+  [CIRCUM]= 0x005E,
+  [SECT]  = 0X00A7,
+  [PLUMIN]= 0X00B1,
+  
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  | Ctrl | LOWER|      |    |      | RAISE| Shift|
- *                  `-------------| Space|    |BckSpc|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
-[_QWERTY] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
+
+[_RSTHD] = LAYOUT( \
+    KC_J,        KC_C,        KC_Y,        KC_F,       KC_K,      KC_Z,  KC_L,        XP(COMA, QUEST),     KC_U,        KC_Q, \
+    SFT_T(KC_R), CTL_T(KC_S), ALT_T(KC_T), GUI_T(KC_H), KC_D,     KC_M,  GUI_T(KC_N), ALT_T(KC_A), CTL_T(KC_I), SFT_T(KC_O),\
+    KC_SCLN,     KC_V,        KC_G,        KC_P,        KC_B,     KC_X,  KC_W,        XP(FULST, EXCLA),      KC_QUOT,      KC_SLSH, \
+             KC_CAPS       ,SFT_T(KC_BSPC), LT(_RAISE, KC_E),     LT(_LOWER, KC_SPC), KC_TAB, TO(RAISE)    \
 ),
 
-/* Raise
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab | Left | Down |  Up  | Right|           |      |   -  |   =  |   [  |   ]  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt |      |           |      |      |      |   \  |   '  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_RAISE] = LAYOUT( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT, \
-                    _______, _______, _______,      _______, _______, _______                    \
+  KC_CLR,    KC_AT,    KC_HASH, KC_DLR,  KC_PERC,       KC_PLUS,  KC_7,    KC_8,   KC_9,   KC_0,    \
+  SFT_T(X(CIRCUM)),   KC_AMPR,  KC_TILD, KC_PIPE, KC_BSLS,      KC_MINS,  KC_4,    KC_5,   KC_6, SFT_T(KC_EQL), \
+  XP(SECT, PLUMIN), _______, KC_UNDS, KC_GRV,  KC_PDOT,       KC_ASTR, KC_1,     KC_2,   KC_3, _______, \
+                        _______, _______, _______,       KC_ENT,   MO(_ADJUST), TO(LOWER)                    \
 ),
 
-/* Lower
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   ~  |      |      |      |           |      |      |      |   |  |   "  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|  Del |
- *                  `-------------|      |    | Enter|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
+
 [_LOWER] = LAYOUT( \
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
-  KC_ESC,  _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
-  KC_CAPS, KC_TILD, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, \
-                    _______, _______, _______,      KC_ENT,  _______, KC_DEL                    \
+  KC_PSCR,   KC_HOME, KC_UP, KC_END,     _______,       KC_LBRC, XP(LTBRK, MORTHN), _______, XP(RTBRK, LESTHN), KC_RBRC,    
+  SFT_T(KC_VOLU),  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGUP,KC_BRMU, _______, _______,  _______, KC_RSFT, \
+  KC_VOLD,  KC_MPRV, KC_MPLY, KC_MNXT,  KC_PGDN,       KC_BRMD, _______, _______,  _______,  _______, \
+                       _______, KC_DEL, KC_ESC ,        _______,  _______,  TO(ADJUST)                    \
 ),
 
-/* Adjust (Lower + Raise)
- *
- * ,----------------------------------.           ,----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |      |      |           |      |      |      |Taskmg|caltde|
- * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|      |      |      |      |           |      |      |      |      |      |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_ADJUST] =  LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, TSKMGR, CALTDEL, \
-  RESET,   _______, _______, _______, _______,      _______, _______, _______, _______,  _______, \
-                    _______, _______, _______,      _______,  _______, _______                    \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10, \
+  _______, DSK_LFT, _______, DSK_RT,  _______,      TSKMGR,  CALTDEL,  _______, KC_F11,   KC_F12, \
+  RESET,   _______, _______, _______, _______,      _______, _______,  _______, _______,  _______, \
+                    _______, _______, _______,      _______, _______,  TO(RSTHD)                    \
 )
+
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
-        persistant_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
+void eeconfig_init_user(void) {
+  set_unicode_input_mode(UC_OSX);
 }
