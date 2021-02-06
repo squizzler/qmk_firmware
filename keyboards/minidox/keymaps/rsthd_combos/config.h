44b21171ecfabd42f4f6a33d649c3383e45fdebc
squizzler <56064906+squizzler@users.noreply.github.com>
2021-02-06 20:28:19 +0000

    Full rewrite with more combos alowing deletion of 'adjust' layer.

--- keyboards/minidox/keymaps/rsthd_combos/config.h
+++ keyboards/minidox/keymaps/rsthd_combos/config.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#define USE_SERIAL
-#define EE_HANDS
 #define MASTER_RIGHT
 
-#define COMBO_COUNT 10
+#define COMBO_COUNT 11
 #define COMBO_TERM 100
 #define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/minidox/keymaps/rsthd_combos/keymap.c
+++ keyboards/minidox/keymaps/rsthd_combos/keymap.c
@@ -3,9 +3,8 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _RSTHD,
-    _LOWER,
-    _RAISE,
-    _ADJUST
+    _FUN,
+    _UPPER
 };
 
 enum combo_events {
@@ -16,9 +15,10 @@ enum combo_events {
   BOT_L,
   BOT_R,
   HN_QUOT,
-  TOP_CTR,
-  MID_CTR,
-  BOT_CTR,
+  FUN_TOP_L,
+  FUN_TOP_R,
+  FUN_MID_L,
+  FUN_MID_R,
 };
 
 const uint16_t PROGMEM fk_combo[] = {KC_F, KC_K, COMBO_END};
@@ -28,9 +28,10 @@ const uint16_t PROGMEM mn_combo[] = {KC_M, KC_N, COMBO_END};
 const uint16_t PROGMEM pb_combo[] = {KC_P, KC_B, COMBO_END};
 const uint16_t PROGMEM xw_combo[] = {KC_X, KC_W, COMBO_END};
 const uint16_t PROGMEM hn_combo[] = {KC_H, KC_N, COMBO_END};
-const uint16_t PROGMEM kz_combo[] = {KC_K, KC_Z, COMBO_END};
-const uint16_t PROGMEM dm_combo[] = {KC_D, KC_M, COMBO_END};
-const uint16_t PROGMEM bx_combo[] = {KC_B, KC_X, COMBO_END};
+const uint16_t PROGMEM fun_fk_combo[] = {KC_9, KC_PLUS, COMBO_END};
+const uint16_t PROGMEM fun_zl_combo[] = {KC_PGUP, KC_HOME, COMBO_END};
+const uint16_t PROGMEM fun_hd_combo[] = {KC_6, KC_EQL, COMBO_END};
+const uint16_t PROGMEM fun_mn_combo[] = {KC_PGDN, KC_LEFT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [TOP_L] = COMBO(fk_combo, KC_EXLM),
@@ -40,9 +41,10 @@ combo_t key_combos[COMBO_COUNT] = {
   [BOT_L] = COMBO(pb_combo, KC_HASH),
   [BOT_R] = COMBO(xw_combo, KC_AMPR),
   [HN_QUOT] = COMBO(hn_combo, KC_QUOT),
-  [TOP_CTR] = COMBO_ACTION(kz_combo),
-  [MID_CTR] = COMBO_ACTION(dm_combo),
-  [BOT_CTR] = COMBO_ACTION(bx_combo),
+  [FUN_TOP_L] = COMBO_ACTION(fun_fk_combo),
+  [FUN_TOP_R] = COMBO_ACTION(fun_zl_combo),
+  [FUN_MID_L] = COMBO(fun_hd_combo, KC_LBRC),
+  [FUN_MID_R] = COMBO(fun_mn_combo, KC_RBRC),
 };
 
   void process_combo_event(uint16_t combo_index, bool pressed) {
@@ -52,19 +54,14 @@ combo_t key_combos[COMBO_COUNT] = {
         tap_code16(LALT(KC_3));
       }
       break;
-      case TOP_CTR:
+      case FUN_TOP_L:
         if (pressed) {
-          set_oneshot_mods(MOD_LGUI);
+          tap_code16(LALT(KC_2));
         }
         break;
-      case MID_CTR:
+      case FUN_TOP_R:
         if (pressed) {
-          set_oneshot_mods(MOD_LALT);
-        }
-        break;
-      case BOT_CTR:
-        if (pressed) {
-          set_oneshot_mods(MOD_LCTL);
+          tap_code16(LALT(KC_4));
         }
         break;
     }
@@ -85,25 +82,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_J,    KC_C,    KC_Y,     KC_F, KC_K,              KC_Z,  KC_L,   KC_COMM,  KC_U,   KC_Q,
     KC_R,    KC_S,    KC_T,     KC_H, KC_D,              KC_M,  KC_N,   KC_A,     KC_I ,  KC_O,
     LCTL_T(KC_SLSH), LALT_T(KC_V), LGUI_T(KC_G), KC_P, KC_B,      KC_X,  KC_W, RGUI_T(KC_DOT), RALT_T(KC_SCLN),RCTL_T(KC_MINS),
-          KC_LSPO, LT(_LOWER, KC_BSPC), KC_E,      KC_SPC, LT(_RAISE, KC_ENT),  KC_RSPC
+          KC_LSPO, LT(_UPPER, KC_BSPC), KC_E,      KC_SPC, LT(_FUN, KC_ENT),  KC_RSPC
 ),
-[_RAISE] = LAYOUT(
-  KC_PAST, KC_7,    KC_8,   KC_9,  KC_PLUS,       KC_NUBS, LALT(KC_2), _______, LALT(KC_4), KC_DLR,
-  KC_CIRC, KC_4,    KC_5,   KC_6,    KC_EQL,      LSFT(KC_NUBS), KC_GRV,   KC_TILD,  KC_PIPE, KC_BSLS,
-  _______, RALT_T(KC_1), KC_2, KC_3,KC_PERC,      _______, _______,    _______, _______, _______,
-                  _______, MO(_ADJUST), KC_0,     _______, _______, _______
+[_FUN] = LAYOUT(
+  KC_BSLS, KC_7,    KC_8,   KC_9,  KC_PLUS,       KC_PGUP,  KC_HOME, KC_UP,   KC_END, KC_DLR,
+  KC_PIPE, KC_4,    KC_5,   KC_6,    KC_EQL,      KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PERC,
+  _______, LALT_T(KC_1), LGUI_T(KC_2), KC_3,KC_PAST,    _______, _______, _______, _______, _______,
+                  _______, _______, KC_0,     _______, _______, _______
 ),
-[_LOWER] = LAYOUT(
-   _______, KC_LBRC, KC_VOLU, KC_RBRC, KC_BRMU,         KC_PGUP,  KC_HOME, KC_UP,   KC_END,  _______,
-   KC_ESC, KC_LPRN,  KC_VOLD, KC_RPRN, KC_BRMD,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, _______,
-   _______, _______, KC__MUTE, _______, _______,         _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,
-                      _______, _______, _______,        KC_DEL,  MO(_ADJUST),  _______
+[_UPPER] = LAYOUT(
+   KC_CIRC, KC_TILD, KC_VOLU, KC_BRMU, LSFT(KC_NUBS), KC_NUBS,  KC_F7, KC_F8, KC_F9, KC_F10,
+   KC_ESC, KC_GRV,  KC_VOLD, KC_BRMD, DSK_LFT,        DSK_RT, KC_F4, KC_F5,  KC_F6, KC_F11,
+   KC_MPRV, KC_MPLY, KC__MUTE, KC_MNXT, _______,      _______, KC_F1, KC_F2, KC_F3, KC_F12,
+                      _______, _______, _______,      KC_DEL,  _______,  _______
 ),
-[_ADJUST] =  LAYOUT(
-  _______,     KC_F7, KC_F8, KC_F9,  SCRCAP,        _______, KC_F10,  KC_F11, KC_F12, _______,
-  _______,     KC_F1, KC_F2, KC_F3, _______,        _______, KC_F4, KC_F5,  KC_F6, _______,
-  OSM(MOD_LCTL),_______, _______, _______, RESET,    KILL, _______, _______,  _______, OSM(MOD_RCTL),
-                    _______, _______, _______,       _______, _______,  _______
-)
-
 };
